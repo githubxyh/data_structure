e7dd6af04632d3b693739c0de0aaa66ac894fd15
xuyh <xyh930929@163.com>
2020-04-21 15:22:04 +0800

    基于链表实现栈结构

--- src/stack/LinkStack.java
+++ src/stack/LinkStack.java
@@ -1,6 +1,54 @@
 package stack;
 
+import link.Node;
+
 public class LinkStack {
 
+    private Node top = null;
 
+    public void push(int data){
+        Node node = new Node(data);
+        if(top == null){
+            top = node;
+            return;
+        }
+        Node temp = top;
+        top = node;
+        node.next = temp;
+    }
+
+    public int pop() {
+        if(top == null){
+            return 0;
+        }
+        Node node = top;
+        top = top.next;
+        return node.data;
+    }
+
+    public boolean hasNext(){
+        if(top == null){
+            return false;
+        }
+        //第一个元素
+        if(top != null && top.next == null) {
+            return true;
+        }
+        return top.next != null;
+    }
+
+    public void testLinkStack() {
+        LinkStack ls = new LinkStack();
+        ls.push(1);
+        ls.push(2);
+        ls.push(3);
+        ls.push(4);
+        ls.push(5);
+        ls.push(6);
+        ls.push(7);
+
+        while (ls.hasNext()){
+            System.out.println(ls.pop());
+        }
+    }
 }
--- src/stack/Main.java
+++ src/stack/Main.java
@@ -2,7 +2,12 @@ package stack;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStack as = new ArrayStack(9);
-        as.testArrayStack();
+        //数组实现栈结构
+        //ArrayStack as = new ArrayStack(9);
+        //as.testArrayStack();
+
+        //链表实现栈结构
+        LinkStack ls = new LinkStack();
+        ls.testLinkStack();
     }
 }
